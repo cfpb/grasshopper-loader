5bc424aed333514b753113439898266cb4090c9f
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-03-04 15:15:47 -0500

    New structure

--- grasshopper-loader.js
+++ grasshopper-loader.js
@@ -2,10 +2,15 @@
 
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+
 var program = require('commander');
+var unzip = require('unzip');
+
 var checkUsage = require('./lib/checkUsage');
-var validateShp = require('./lib/validateShp');
-var shp = require('./lib/shp');
+var project = require('./lib/project');
+var esLoader = require('./lib/esLoader');
 
 program
   .version('1.0.0')
@@ -17,11 +22,29 @@ program
 
 if(!checkUsage(program)) return;
 
+var shapefile = program.shapefile;
+var ext = path.extname(shapefile);
+var basename = path.basename(shapefile, ext);
+var dirname = path.dirname(shapefile);
 
-validateShp(program.shapefile,function(err, shapefile){
-  if (err) return console.log(err);
-  shp.read(shapefile, program.host, program.port);
-});
+var shp = path.join(dirname, basename + '.shp');
+var prj = path.join(dirname, basename + '.prj');
+
+
+if(ext.toLowerCase() === '.zip'){
+  fs.createReadStream(shapefile).pipe(unzip.Extract({path: dirname}))
+    .on('close', processShapefile);
+}else{
+  processShapefile(); 
+}
+
+function processShapefile(){
+  fs.readFile(prj, function(err, data){
+    if (err) throw err;
+    project(shp, prj);
+  });
+}
+//  esLoader.read(shapefile, program.host, program.port);
 
 
 
