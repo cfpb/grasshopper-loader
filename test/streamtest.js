b112c7e4e0b8b2a03c51d7b61387568a336f2157
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-03-05 22:42:16 -0500

    ogr json splitter

--- test/streamtest.js
+++ test/streamtest.js
@@ -1,5 +1,6 @@
 var ogr = require('../lib/ogr');
 var transformer = require('../transforms/arkansas');
 var fs = require('fs');
+var split = require('split');
 
-ogr('t.json').pipe(transformer()).pipe(fs.createWriteStream('op.txt'));
+ogr('t.json').pipe(split(/,\r?\n/)).pipe(transformer()).pipe(fs.createWriteStream('op.txt'));
