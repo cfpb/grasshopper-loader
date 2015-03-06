6257afcae44024b0f3c45a23045484451cde60d9
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-03-06 14:40:34 -0500

    Testing

---
+++ transformers/arkansas.js
@@ -0,0 +1,22 @@
+var through = require('through2');
+var makeAddress = require('../lib/makeAddress');
+
+function transform(chunk, enc, cb){
+  chunk = JSON.parse(chunk);
+  var trimmed = {};
+  var props = chunk.properties;
+
+  trimmed.address = makeAddress(props.ADR_LABEL,
+                                props.ADR_CITY,
+                                props.ADR_STATE,
+                                props.ADR_ZIP5
+                                );
+  trimmed.coordinates = chunk.geometry.coordinates
+
+  this.push(JSON.stringify(trimmed));
+  cb();
+}
+
+module.exports = function(){
+  return through(transform);
+};
