1a4a89db2125770643cbcb3929f507453d57c929
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-03-12 03:54:07 -0400

    checkUsage tested

--- test/test.js
+++ test/test.js
@@ -11,6 +11,24 @@ var esLoader = require('../lib/esLoader');
 
 
 test('Check Usage', function(t){
-  t.plan(5);
-  t.equal(2+2, 4);
+  t.plan(6);
+
+  var program = {
+    shapefile: 'someshape',
+    host: 'localhost',
+    port: 9200,
+    transformer: './transformers/default'
+  }
+
+  var first = checkUsage(program)
+  t.equal(first.messages.length, 3);
+  t.equal(first.err, 0);
+  
+  var second = checkUsage({port:9201});
+  t.equal(second.messages.length, 1);
+  t.equal(second.err, 1);
+
+  var third = checkUsage({port:NaN})
+  t.equal(third.messages.length, 2);
+  t.equal(third.err, 1);
 });
