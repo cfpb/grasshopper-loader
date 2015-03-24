bbdc848291e90e6c82a225e98dd19fd3761cd9e7
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-03-23 21:14:10 -0400

    Error condition on invalid file

--- test/dev/wrapVerify.js
+++ test/dev/wrapVerify.js
@@ -1,8 +1,8 @@
 var verify = require('../../lib/verify');
 
 function log(err){
-  if(err) throw new Error('Expected: ' + err.expected + '\nActual: ' + err.actual + '\n');
+  if(err) throw err.error;
   console.log('GREAT');
 }
 
-verify('../data/t.json', 10, log);
+verify('../data/t.jsn', 10, log);
