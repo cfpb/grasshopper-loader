1f0407fd07d93486da42b593bf30acddbfc6a6c6
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-04-14 00:47:43 -0400

    tar f option unneeded on pipe

--- install_gdal.sh
+++ install_gdal.sh
@@ -5,13 +5,13 @@ sudo apt-add-repository ppa:chris-lea/node.js -y
 sudo apt-get update -q
 sudo apt-get -y install nodejs g++ build-essential git curl
 
-curl http://download.osgeo.org/gdal/1.11.2/gdal-1.11.2.tar.gz | tar xfz 
+curl http://download.osgeo.org/gdal/1.11.2/gdal-1.11.2.tar.gz | tar xz 
 cd gdal-1.11.2
 ./configure && make && sudo make install
 sudo ldconfig
 cd ..
 
-curl http://download.osgeo.org/proj/proj-4.9.1.tar.gz | tar xfz 
+curl http://download.osgeo.org/proj/proj-4.9.1.tar.gz | tar xz 
 cd proj-4.9.1
 ./configure && make && sudo make install
 sudo ldconfig
