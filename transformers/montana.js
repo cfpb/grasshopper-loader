eeb78e14f8e7bbd175f6773b2a527d0697d2f096
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-05-08 17:14:25 -0400

    update montana to use new transformerTemplate

--- transformers/montana.js
+++ transformers/montana.js
@@ -1,5 +1,5 @@
 'use strict';
-var template = require('../lib/transformerTemplate');
+var template = require('../lib/pointTransformer');
 module.exports = template(function(props){
   return props.Number + ' ' + props.Address
 }, 'City', 'State', 'ZIP');
