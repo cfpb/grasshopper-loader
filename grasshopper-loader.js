19bb73539dfc9a86bf381e3a64d9ade76d94c316
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-05-16 12:09:40 -0400

    Use linked ES container as host

--- grasshopper-loader.js
+++ grasshopper-loader.js
@@ -25,13 +25,23 @@ var Counter = require('./lib/counter');
 //Favor source GDAL installations for ogr transformations
 process.env.PATH = '/usr/local/bin:' + process.env.PATH
 
+var esVar = process.env.ELASTICSEARCH_PORT;
+var esHost;
+var esPort;
+
+if(esVar){
+  esVar = esVar.split('//')[1].split(':');
+  esHost = esVar[0];
+  esPort = +esVar[1];
+}
+
 program
   .version('0.0.1')
   .option('-b, --bucket <bucket>', 'An S3 bucket where data resides. If no -d option is passed, will attempt to load all data in the bucket.')
   .option('-d, --data <data>', 'Point data as a .zip, .shp, .gdb, or directory. Provide a local or an S3 key. Zip an GeoJson data can also be accessed via url. Required if no bucket is passed.')
   .option('-t, --transformer <transformer>', 'Data transformer. Defaults to ./transformers/[[file basename]].js.')
-  .option('-h, --host <host>', 'ElasticSearch host. Defaults to localhost', 'localhost')
-  .option('-p, --port <port>', 'ElasticSearch port. Defaults to 9200.', Number, 9200)
+  .option('-h, --host <host>', 'ElasticSearch host. Defaults to localhost unless linked to a Docker container aliased to Elasticsearch', esHost || 'localhost')
+  .option('-p, --port <port>', 'ElasticSearch port. Defaults to 9200 unless linked to a Docker container aliased to Elasticsearch.', Number, esPort || 9200)
   .option('-l, --log <log>', 'ElasticSearch log level. Defaults to debug.', 'debug')
   .option('--index <index>', 'Elasticsearch index. Defaults to address.', 'address')
   .option('--type <type>', 'Elasticsearch type within the provided or default index. Defaults to point.', 'point')
