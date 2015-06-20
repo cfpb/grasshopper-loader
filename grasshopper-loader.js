56dacaf9b2c852c0e5f9b50554f314df67ef3fca
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-06-20 12:09:09 -0400

    disallow unformatted csv

--- grasshopper-loader.js
+++ grasshopper-loader.js
@@ -3,7 +3,6 @@
 'use strict';
 
 var fs = require('fs-extra');
-
 var isUrl = require('is-url');
 var pump = require('pump');
 var lump = require('lump-stream');
@@ -41,7 +40,7 @@ if(require.main === module){
   program
     .version('0.0.1')
     .option('-b, --bucket <bucket>', 'An S3 bucket where data resides. If no -d option is passed, will attempt to load all data in the bucket.')
-    .option('-d, --data <data>', 'Point data as a .zip, .shp, .gdb, or directory. Provide a local or an S3 key. Zip an GeoJson data can also be accessed via url. Required if no bucket is passed.')
+    .option('-d, --data <data>', 'Point data as a .zip, .shp, .gdb, GeoJson, or directory. Will also accept a .csv if preformatted by ogr2ogr. Provide a local file or an S3 key. Zipped, gzipped, and GeoJson data can also be accessed via url. Required if no bucket is passed.')
     .option('-t, --transformer <transformer>', 'Data transformer. Defaults to ./transformers/[[file basename]].js.')
     .option('-h, --host <host>', 'ElasticSearch host. Defaults to localhost unless linked to a Docker container aliased to Elasticsearch', esHost || 'localhost')
     .option('-p, --port <port>', 'ElasticSearch port. Defaults to 9200 unless linked to a Docker container aliased to Elasticsearch.', Number, esPort || 9200)
@@ -50,7 +49,7 @@ if(require.main === module){
     .option('--type <type>', 'Elasticsearch type within the provided or default index. Defaults to point.', 'point')
     .option('--profile <profile>', 'The aws credentials profile in ~/.aws/credentials. Will also respect AWS keys as environment variables.', 'default')
     .option('--source-srs <sourceSrs>', 'Source Spatial Reference System, passed to ogr2ogr as -s_srs. Auto-detects by default.')
-    .option('--preformatted', 'Input has been preformatted to GeoJson and transformed to WGS84 by ogr2ogr. Results in the loader skipping ogr2ogr and immediately splitting the input into records.')
+    .option('--preformatted', 'Input has been preformatted to GeoJson or csv and transformed to WGS84 by ogr2ogr. Results in the loader skipping ogr2ogr and immediately splitting the input into records.')
     .parse(process.argv);
 
     run(program);
