038383bafb690c32d7e19d21ea56bd087f12af28
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-09-11 09:21:11 -0700

    Prune old files

--- test/dev/streamtest.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var ogr = require('../../lib/ogr');
-var transformer = require('../../transforms/arkansas');
-var fs = require('fs');
-var splitOGR = require('../../lib/splitOGRJSON');
-ogr('testdata/t.json').pipe(splitOGR()).pipe(transformer()).pipe(process.stdout);
