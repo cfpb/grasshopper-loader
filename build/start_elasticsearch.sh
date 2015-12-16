405713d7a2a857d37a7fe087d80502a9ed666e4d
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-12-15 23:42:34 -0800

    pass proper host

--- build/start_elasticsearch.sh
+++ build/start_elasticsearch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CONTAINER_ID=$(docker run -d -p 9200:9200 -p 9300:9300 elasticsearch:1.7.3)
-ESHOST=$(docker inspect "$CONTAINER_ID" | grep IPAddress | cut -d '"' -f 4)
-echo "$ESHOST"
+ES_HOST=$(docker inspect "$CONTAINER_ID" | grep IPAddress | cut -d '"' -f 4)
+echo "$ES_HOST"
