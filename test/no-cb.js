9a5b9ac2c747db97768d21333045ff6bbec8b492
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-12-30 12:27:41 -0800

    introduce cli concurrency

--- test/no-cb.js
+++ test/no-cb.js
@@ -32,6 +32,7 @@ options
   .option('-t, --type <type>', 'Elasticsearch type within the provided or default alias. Defaults to point.', 'point')
   .option('-l, --log <log>', 'ElasticSearch log level. Defaults to error.', 'error')
   .option('-q, --quiet', 'Suppress logging.', false)
+  .option('-c, --concurrency <concurrency>', 'How many loading tasks will run at once. Defaults to 2.', 2)
   .option('-b, --bucket <bucket>', 'An S3 bucket where the data resides.')
   .option('-d, --directory <directory>', 'A directory where data sources reside, either relative to the current folder or the passed S3 bucket.')
   .option('-P, --profile <profile>', 'The aws profile in ~/.aws/credentials. Only needed if loading data from a bucket. AWS environment variables will override this value.', 'default')
